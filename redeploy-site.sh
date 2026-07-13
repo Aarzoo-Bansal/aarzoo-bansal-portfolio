97c6fdbbc0f0249b0646aa842afdee33d27023a2
Aarzoo Bansal <aarzoo1701@gmail.com>
2026-07-12 20:03:49 -0700

    Update redeploy script to use systemd instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-# 1. Kill all existing tmux sessions
-tmux kill-server 2>/dev/null || true
-
-# 2. cd into the project folder
+# 1. cd into the project folder
 cd ~/aarzoo-bansal-portfolio
 
-# 3. Get the latest changes from main
+# 2. Get the latest changes from main
 git fetch && git reset origin/main --hard
 
-# 4. Enter venv and install dependencies
+# 3. Enter venv and install dependencies
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# 5. Start a new detached tmux session: cd, activate venv, start Flask
-tmux new-session -d -s flask "cd ~/aarzoo-bansal-portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+# 4. Restart the myportfolio service
+systemctl restart myportfolio
+
