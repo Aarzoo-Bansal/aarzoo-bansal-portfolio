0487d3dfc5242a78b58acc4b18ac895b07face92
Aarzoo Bansal <aarzoo1701@gmail.com>
2026-07-27 14:05:10 -0700

    Updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,10 +7,8 @@ cd ~/aarzoo-bansal-portfolio
 # 2. Get the latest changes from main
 git fetch && git reset origin/main --hard
 
-# 3. Enter venv and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# 3. Remove the existing containers to avoid out of memory issue
+docker compose -f docker-compose.prod.yml down
 
-# 4. Restart the myportfolio service
-systemctl restart myportfolio
-
+# 4. Spin up new containers
+docker compose -f docker-compose.prod.yml up -d --build
